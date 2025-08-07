c52de392e7ef2d6bba39fe31d751c1dd226c131e
Jhulan Dey <jhulandey@gmail.com>
2025-08-07 07:40:31 +0530

    Add intro and variables/data types example files
    Added initial HTML and JS files for the Intro and Variables and Data Types sections, including basic console output and variable declarations. Also included VS Code settings for live server port configuration.

---
+++ 02_Variables_and_data_types/script.js
@@ -0,0 +1,22 @@
+var nameOn ='Duck';
+let varName = "Variables and Data Types";
+const variableName = 'Hello!';
+
+const letVariable = "let";
+const constVariable = "const";
+
+// const  let = "let";
+// const  const = "const";
+
+const $name = 'John Doe';
+const _name = 'Jane Doe';
+const name = 'Johny Doe';
+
+console.log(nameOn);
+console.log(varName);
+console.log(variableName);
+console.log(letVariable);
+console.log(constVariable);
+console.log($name);
+console.log(_name);
+console.log(name);
